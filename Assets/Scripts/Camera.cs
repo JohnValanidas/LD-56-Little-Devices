8117f0558430505e241494b4baf97be3d800682e
John Thomas Shaw Valanidas <john.valanidas@gmail.com>
2024-10-04 23:15:42 -0400

    basic Camera script

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
-{
+public class Camera : MonoBehaviour {
+
+    public float speed = 5;
+
+    public Transform camera;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +14,12 @@ public class Camera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 vec = new Vector2(moveHorizontal, moveVertical).normalized * speed;
+        camera.Translate(vec * Time.deltaTime);
     }
 }
