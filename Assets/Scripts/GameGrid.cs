05a28141f57fbb701afd47fdd5780180b298bda8
Borin Ouch <borin.ouch@gmail.com>
2024-10-05 14:38:34 -0700

    Update GameGrid class

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameGrid : MonoBehaviour
 {
+    public Vector2Int minTilePos;
+    public Vector2Int maxTilePos;
+
     public GameObject spawner;
     public GameObject target;
 
     private GridLayout grid;
+
+    private Dictionary<Vector3Int, GameObject> staticObjects = new Dictionary<Vector3Int, GameObject>();
 
     private void DrawDebugTile(Vector3 bottomLeft, Vector3 topRight, Color color, float duration)
     {
@@ -16,6 +22,7 @@ public class GameGrid : MonoBehaviour
         Debug.DrawLine(bottomLeft, topLeft, color, duration);
         Debug.DrawLine(bottomLeft, bottomRight, color, duration);
         Debug.DrawLine(topLeft, topRight, color, duration);
+
     }
 
     // Start is called before the first frame update
@@ -23,13 +30,8 @@ public class GameGrid : MonoBehaviour
     {
         Debug.Log("test");
 
-        this.grid = GetComponentInParent<GridLayout>();
-
-        if (!this.grid)
-        {
-            Debug.Log("Grid NOT FOUND");
-        }
-
+        grid = GetComponentInParent<GridLayout>();
+        Debug.Assert(grid, "Grid NOT FOUND!");
 
         var rect = Camera.main.pixelRect;
         Debug.Log($"rect: {rect}");
@@ -45,7 +47,7 @@ public class GameGrid : MonoBehaviour
             {
                 var start = grid.CellToWorld(new Vector3Int(x, y, 0));
                 var end = new Vector3(start.x + cellSize.x, start.y + cellSize.y);
-                DrawDebugTile(start, end, Color.black, 100f);
+                DrawDebugTile(start, end, Color.black, 1f);
             }
         }
     }
@@ -60,11 +62,55 @@ public class GameGrid : MonoBehaviour
             var cellPos = grid.WorldToCell(pos);
             Debug.Log($"Click! {cellPos.x}, {cellPos.y}");
 
-            var tileCenter = grid.CellToWorld(cellPos) + (grid.cellSize / 2);
-            var instance = Instantiate(spawner, tileCenter, Quaternion.identity);
+            if (!ExistsAtCell(cellPos))
+            {
+                var instance = InstantiateAtCell(cellPos);
 
-            var spawnerComponent = instance.GetComponent<Spawner>();
-            spawnerComponent.target = target.transform;
+                var component = instance.GetComponent<Spawner>();
+                component.target = target.transform;
+            } else
+            {
+                DestroyAtCell(cellPos);
+            }
         }
+    }
+
+    public bool ExistsAtCell(Vector3Int cellPos)
+    {
+        return staticObjects.ContainsKey(cellPos);
+    }
+
+    public GameObject InstantiateAtCell(Vector3Int cellPos, bool isStatic = true)
+    {
+        var tileCenter = grid.CellToWorld(cellPos) + (grid.cellSize / 2);
+        var instance = Instantiate(spawner, tileCenter, Quaternion.identity);
+
+        if (isStatic)
+        {
+            staticObjects.Add(cellPos, instance);
+        }
+
+        return instance;
+    }
+
+    public bool DestroyAtCell(Vector3Int cellPos)
+    {
+        Debug.Assert(staticObjects.ContainsKey(cellPos), $"No objects at cell position: {cellPos}");
+
+        var gameObject = staticObjects[cellPos];
+        Destroy(gameObject);
+        staticObjects.Remove(cellPos);
+
+        return true;
+    }
+
+    public bool InGridBounds(Vector3Int cellPos)
+    {
+        if (cellPos.x < minTilePos.x || cellPos.x > maxTilePos.x || cellPos.y < minTilePos.y || cellPos.y > maxTilePos.y)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
